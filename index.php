a3a40de83b211a07bbe3eb8d286ce12a91744e4c
wuminglan <1037279225@qq.com>
2017-07-13 09:58:42 +0800

    初步始化的工作

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+header("content-type:text/html;charset=utf8");
+
+ 
+// include '/views/dashboard/index.html';
+// var_dump($_SERVER); 
+
+ $path = $_SERVER['PATH_INFO'];
+//   var_dump($path);
+ 
+
+?>
